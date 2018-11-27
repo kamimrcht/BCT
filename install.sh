04151d4400ad405dbe6b5a41d46d15edc76706e2
Malfoy <antoine.limasset@gmail.com>
2018-11-27 16:05:12 +0100

    first commit

---
+++ install.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+
+
+#THIS INSTALLATION REQUIRES GCC, GIT, MAKE, CMAKE
+
+
+
+function help {
+echo "Btt installation script"
+echo "This installation requires GCC>=4.9, GIT, MAKE and CMAKE3"
+echo "-f absolute path of folder to put the binaries"
+echo "-t to use multiple thread for compilation (default 8)"
+}
+
+
+#~ mkdir src;
+#~ cd src;
+
+threadNumber=8
+folder=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+folder+="/bin"
+
+
+while getopts "hf:t:" opt; do
+case $opt in
+h)
+help
+exit
+;;
+f)
+echo "use folder: $OPTARG" >&2
+folder=$OPTARG
+;;
+s)
+echo "use  $OPTARG threads" >&2
+threadNumber=$OPTARG
+;;
+\?)
+echo "Invalid option: -$OPTARG" >&2
+exit 1
+;;
+:)
+echo "Option -$OPTARG requires an argument." >&2
+exit 1
+;;
+esac
+done
+
+if [ -z "$folder"  ]; then
+	help
+	exit 0
+	fi
+
+
+
+mkdir $folder;
+echo "I put binaries in $folder";
+
+
+cat src/btt_header.py>Btt.py
+echo "Btt_INSTDIR = (\"$folder\")" >> Btt.py
+cat src/btt_broken.py>>Btt.py
+chmod +x Btt.py
+
+
+
+
+
+git clone --recursive https://github.com/GATB/bcalm >>logCompile 2>>logCompile;
+cd bcalm;
+mkdir build; cd build;
+cmake -DKSIZE_LIST="32 64 128" ..  >>logCompile 2>>logCompile;
+make -j $threadNumber >>logCompile 2>>logCompile;
+cp bcalm $folder;
+cd ../..;
+echo PHASE TWO, GRAPH CONSTRUCTION: BCALM;
+
+
+
+git clone https://github.com/Malfoy/BGREAT2 >>logCompile 2>>logCompile;
+cd BGREAT2;
+make -j $threadNumber >>logCompile 2>>logCompile;
+cp bgreat $folder;
+cd ..;
+echo PHASE THREE, READ MAPPING ON THE DBG: BGREAT;
+
+
+git clone https://github.com/Malfoy/BTT >>logCompile 2>>logCompile;
+cd BTT;
+make -j $threadNumber >>logCompile 2>>logCompile;
+if [ $? -ne 0 ]
+       then
+              echo "there was a problem with btrim compilation, check logs"
+              exit 1
+       fi
+cp btt $folder;
+cd ..;
+echo PHASE FOUR GRAPH CLEANING: BTT;
+
+
+
+
+echo The end !;
